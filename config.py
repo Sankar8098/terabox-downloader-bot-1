bfdc0a80397d881f4c7e91c1f9e8c7d1b8a68f81
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-06-18 18:36:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,23 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+import os
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+# Telegram API credentials
+API_ID = os.getenv('API_ID', '23990433')  # Replace with your API ID
+API_HASH = os.getenv('API_HASH', 'e6c4b6ee1933711bc4da9d7d17e1eb20')  # Replace with your API Hash
+BOT_TOKEN = os.getenv('BOT_TOKEN', '5811431199:AAGad6YjD5nC9PTgurWIbRM4krUpopevpf8')  # Replace with your bot token
 
+# Redis database credentials
+REDIS_HOST = os.getenv('REDIS_HOST', 'redis-15392.c304.europe-west1-2.gce.cloud.redislabs.com')  # Replace with your Redis host
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))  # Default Redis port is 6379
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', 'cbw7xHuW3uJmddWJRRqxiSWmRvKi81GM')  # Replace with your Redis password
 
-## REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+# Private chat ID where media files will be sent and stored
+PRIVATE_CHAT_ID = os.getenv('PRIVATE_CHAT_ID', '-1001571491517')  # Replace with your private chat ID
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+# List of admin user IDs who can use admin commands
+ADMINS = list(map(int, os.getenv('ADMINS', '5821871362,987654321').split(',')))  # Replace with Telegram user IDs of the admins
+
+# Other settings (if any)
+# Add any other configuration variables you need here
+
+# Config
+COOKIE = os.getenv("COOKIE", "csrfToken=BnQx-ffuJKamkWLrsx3Ksds4; browserid=qEGrcJcpudjTVSp34yywiOK8KGczvjGYTPI6jJMydd7dpoQVUXCzDlp7JFM=; lang=en; __bid_n=18db31bdad1e0ee8094207; __stripe_mid=a300821a-4b38-4e0a-a227-5c27f1b7c54789f06e; __stripe_sid=f7b244b5-18a8-4540-bfa0-222b4acc0bf5eb56ba; ndus=Yb47bKTteHuiwDuAJYscQHtlthgbg2-2R4yRRWFe; ndut_fmt=17DAF2FD0DC6DF723D10EB645D49FD8581EFDCCB4A2823D11359EA0CCE270FBE")
